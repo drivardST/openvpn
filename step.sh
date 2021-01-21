c1f96682221f17de60bfffefec362d1ded72e366
Dennis Rivard <23558341+dennisrivard@users.noreply.github.com>
2021-01-21 10:47:12 -0500

    Update step.sh

--- step.sh
+++ step.sh
@@ -43,6 +43,9 @@ EOF
     echo ${client_crt} | base64 -D -o client.crt
     echo ${client_key} | base64 -D -o client.key
 
+    echo "My public IP Address:"
+    curl ipinfo.io/ip
+
     sudo openvpn --version
 #    sudo openvpn --compress --client --dev tun --proto udp --remote {$host} {$port} --resolv-retry infinite --nobind --persist-key --persist-tun --comp-lzo --verb 3 --ca ca.crt --cert client.crt --key client.key --cipher AES-256-CBC --data-ciphers AES-256-GCM:AES-128-GCM:AES-256-CBC&
     sudo openvpn --client --dev tun --proto udp --remote {$host} {$port} --resolv-retry infinite --nobind --persist-key --persist-tun --cipher AES-256-CBC --data-ciphers AES-256-GCM:AES-128-GCM:AES-256-CBC --comp-lzo --verb 9 --tls-client --verify-x509-name sc5-cicd-ovpn-1.squaretrade.com name --ca ca.crt --cert client.crt --key client.key &
